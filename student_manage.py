54ceb750d22034356f461c46e83e546865cc95ae
tumana777 <tumana991@gmail.com>
2024-02-28 17:54:13 +0400

    Make input validations

--- student_manage.py
+++ student_manage.py
@@ -16,48 +16,68 @@ def validate_name(name):
 
 # Validate unique number
 def validate_number(student_list, number):
-    
+    # Checking if number is integer
     if not number.isdigit():
+        # If number is not integer return false
             print("Please, input valid number!")
             return False
+    # Checking if student number is already in list or not
     for student in student_list:
                 if student.roll_number == number:
+                    # If student number is already in list, returns false
                     print("Roll number is already in list!")
                     return False
+    # If everything is validated, function returns true
     return True
 
 # Validate student grade
 def validate_grade(grade):
+    # Create possible grades list
     grades = ["A", "B", "C", "D", "E", "F"]
     if not grade in grades:
+        # Checking if inputted grade is not in list, return false
         print("Please, input valid grade!")
         return False
+    # If everything is validated, function returns true
     return True
 
 # Validate Student Info
 def validate_student(student_list):
     counter = 0
+    # This while loop gives user 3 tries to input valid name
     while counter < 3:
         name = input("Please, input student name: ").strip().title()
+        # Calling this function checks name validation
         if not validate_name(name):
+            # If name not valid, counter increases and prompts user again to input valid name
             counter += 1
             continue
+        # If name is valid, counter will reset, breaks this while loop and begins next while loop
         counter = 0
         break
+    # This while loop gives user 3 tries to input valid roll number
     while counter < 3:
         number = input("Please, input student roll number: ")
+        # Calling this function checks roll number validation
         if not validate_number(student_list, number):
+            # If roll number not valid, counter increases and prompts user again to input valid number
             counter += 1
             continue
+        # If roll number is valid, counter will reset, breaks this while loop and begins next while loop
         counter = 0
         break
+    # This while loop gives user 3 tries to input valid grade
     while True:
         grade = input("Please, input student grade: ").upper()
+        # Calling this function checks grade validation
         if not validate_grade(grade):
+            # If grade not valid, counter increases and prompts user again to input valid grade
             counter += 1
+            # If user inputs not valid grade 3 times, this function will stop
             if counter == 3:
                 return
             continue
+        # If grade is valid, breaks this while loop
         break
         
     return name, number, grade
